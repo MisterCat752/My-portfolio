846b3863d62b26f12d2ad719242cb0e5ca7af213
Nick <nikitareznov@gmail.com>
2026-02-09 19:34:07 +0200

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './sections/NavBar';
 import Hero from './sections/Hero';
+import About from './sections/About';
 
 function App() {
   return (
     <div className='container mx-auto max-w-7xl h-[2000px]'>
       <Navbar />
-      <Hero />/{/* <About /> */}
+      <Hero />
+      <About />
       {/* <Projects /> */}
       {/* <Experiences /> */}
       {/* <Contact /> */}
